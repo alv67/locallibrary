90441e34a1b14ea7c36a71adb6eb012bdab11835
AMeLaMela <alv67.web@gmail.com>
2018-05-28 02:12:47 +0200

    Django Admin Site

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
 # --- GENRE MODEL ---
 class Genre(models.Model):
@@ -16,8 +17,18 @@ class Genre(models.Model):
         """
         return self.name
 
+# --- LANGUAGE MODEL ---
+class Language(models.Model):
+    """
+    Model representing a Language (e.g. English, French, Japanese, etc.)
+    """
+    name = models.CharField(max_length=200, help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
 
-from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
 
 #--- BOOK MODEL ---
 class Book(models.Model):
@@ -33,6 +44,7 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """
@@ -46,6 +58,13 @@ class Book(models.Model):
         Returns the url to access a detail record for this book.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 # --- BOOK INSTANCE MODEL ---
 import uuid # Required for unique book instances
